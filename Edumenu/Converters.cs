81f19f6faf0c34ac528908f46787728a76d41513
Tuomas Tikkanen <tuomas.tikkanen@hotmail.com>
2015-09-01 01:12:51 +0300

    Added converter to capitalize strings.

---
+++ Edumenu/Converters.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace Converters
+{
+    public class StringToUpperConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value != null && value is string)
+            {
+                return ((string)value).ToUpper();
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            // The value is being binded "OneWay" so ConvertBack will not get called
+            throw new NotImplementedException();
+        }
+    }
+}
