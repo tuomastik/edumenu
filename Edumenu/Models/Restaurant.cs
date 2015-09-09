e9bab22ad68b63ba3e3bb55c229a9c0c242b49fa
Tuomas Tikkanen <t-tuoti@microsoft.com>
2015-09-09 17:30:16 +0300

    Added enum Company and instance variables company and school.

--- Edumenu/Models/Restaurant.cs
+++ Edumenu/Models/Restaurant.cs
@@ -6,10 +6,20 @@ using System.Threading.Tasks;
 
 namespace Edumenu.Models
 {
+    public enum Company
+    {
+        Sodexo,
+        Juvenes,
+        Amica,
+        Campusravita,
+    }
+
     public class Restaurant
     {
         public string name { get; set; }
         public string menu { get; set; }
+        public Company company { get; set; }
+        public School school { get; set; }
         public Uri menuUrl { get; set; }
         public Uri homeUrl { get; set; }
     }
