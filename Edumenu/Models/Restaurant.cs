d6f76f6702446feddc96c24d2de904b193ed4ebf
Tuomas Tikkanen <tuomas.tikkanen@hotmail.com>
2015-09-10 23:54:30 +0300

    Added progress tracking and whitespace removal to PurifyMenu.

--- Edumenu/Models/Restaurant.cs
+++ Edumenu/Models/Restaurant.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.ComponentModel;
+using System.Windows;
 
 namespace Edumenu.Models
 {
@@ -25,8 +28,6 @@ namespace Edumenu.Models
         public Uri menuUrl { get; set; }
         public Uri homeUrl { get; set; }
 
-
-
         internal void ParseAmica(string sourceCode)
         {
             try
@@ -36,27 +37,28 @@ namespace Edumenu.Models
                 bool correctDayFound = false;
 
                 XElement rss = XElement.Parse(sourceCode);
-                var query = rss.Elements().Elements();
-                foreach (XElement ele in query) // Loop through days
+                var days = rss.Elements().Elements();
+                foreach (XElement day in days) // Loop through days
                 {
-                    if (!ele.Name.LocalName.ToString().Contains("item"))
+                    if (!day.Name.LocalName.ToString().Contains("item"))
                     {
                         continue;
                     }
-                    var query2 = ele.Elements();
-                    foreach (var ele2 in query2) // Loop through day attributes
+                    var dayAttributes = day.Elements();
+                    foreach (var dayAttribute in dayAttributes) // Loop through day attributes
                     {
-                        if (ele2.Name.LocalName.ToString().Contains("title"))
+                        if (dayAttribute.Name.LocalName.ToString().Contains("title"))
                         {
-                            if (!ele2.Value.ToString().ToLower().Contains(Globals.selectedDay.ToLower()))
+                            if (!dayAttribute.Value.ToString().ToLower().Contains(Globals.selectedDay.ToLower()))
                             {
-                                continue;
+                                // Skip if not the selected day
+                                break;
                             }
                             correctDayFound = true;
                         }
-                        else if (ele2.Name.LocalName.ToString().Contains("description"))
+                        else if (dayAttribute.Name.LocalName.ToString().Contains("description"))
                         {
-                            restaurantMenu = ele2.Value.ToString();
+                            restaurantMenu = dayAttribute.Value.ToString();
                         }
                         else
                         {
@@ -75,27 +77,72 @@ namespace Edumenu.Models
                 // Silently swallow an error condition and continue execution.
                 // This is okay, because menu strings were initialized properly before execution.
             }
+            finally
+            {
+                UpdateProgress();
+            }
+        }
+
+        private void UpdateProgress()
+        {
+            Globals.nRestaurantsProcessed += 1;
+            int totalSchoolRestaurants = 0;
+            foreach (Restaurant restaurant in App.RestaurantViewModel.restaurantsAll)
+            { 
+                if (restaurant.school.name_fi.Equals(school.name_fi))
+                {
+                    totalSchoolRestaurants += 1;
+                }
+            }
+            if (totalSchoolRestaurants != 0) // Do not divide by zero
+            { 
+                Globals.progress = (int)((double)Globals.nRestaurantsProcessed / (double)totalSchoolRestaurants * 100);
+            }
+            if (Globals.nRestaurantsProcessed.Equals(totalSchoolRestaurants))
+            {
+                Globals.allRestaurantsProcessed = true;
+            }
         }
 
         internal void ParseCampusRavita(string p)
         {
-            //throw new NotImplementedException();
+            for (int i=0; i< 10*10; ++i)
+            {
+                continue;
+            }
+            UpdateProgress();
         }
 
         internal void ParseJuvenes(string p)
         {
-            //throw new NotImplementedException();
+            for (int i = 0; i < 10 *10*10* 10 * 10; ++i)
+            {
+                continue;
+            }
+            UpdateProgress();
         }
 
         internal void ParseSodexo(string p)
         {
-            //throw new NotImplementedException();
+            for (int i = 0; i < 10 * 10 * 10 * 10; ++i)
+            {
+                continue;
+            }
+            UpdateProgress();
         }
 
         private string PurifyMenu(string dirtyMenu)
         {
             dirtyMenu = dirtyMenu.Replace("&lt;br&gt;", "");
             dirtyMenu = dirtyMenu.Replace("<br>", "");
+            // Remove whitespace inside curly and square brackets
+            //  \s +     # Match whitespace
+            //  (?=      # only if followed by...
+            //  [^ ()] * # any number of characters except parentheses
+            //  \)       # and a closing parenthesis
+            //  )        # End of lookahead assertion
+            dirtyMenu = Regex.Replace(dirtyMenu, "\\s+(?=[^()]*\\))", "");
+            //dirtyMenu = Regex.Replace(dirtyMenu, "\\s+(?=[^[]]*\\])", "");
             string cleanMenu = dirtyMenu.Trim();
             return cleanMenu;
         }
