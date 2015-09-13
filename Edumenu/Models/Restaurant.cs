a28f4d6409a0ce832dd5978bafc2a69fd2e229c1
Tuomas Tikkanen <t-tuoti@microsoft.com>
2015-09-13 14:33:21 +0300

    Simplified Amica parse, added Juvenes parse and added CleanMenu function.

--- Edumenu/Models/Restaurant.cs
+++ Edumenu/Models/Restaurant.cs
@@ -32,45 +32,21 @@ namespace Edumenu.Models
         {
             try
             {
-                // Initialize
-                string restaurantMenu = "";
-                bool correctDayFound = false;
+                string dayOfWeek = "";
 
                 XElement rss = XElement.Parse(sourceCode);
-                var days = rss.Elements().Elements();
-                foreach (XElement day in days) // Loop through days
+                var dayNodes = rss.Element("channel").Elements("item");
+                foreach (XElement dayNode in dayNodes)
                 {
-                    if (!day.Name.LocalName.ToString().Contains("item"))
+                    dayOfWeek = dayNode.Element("title").ToString().ToLower();
+                    if (!dayOfWeek.Contains(Globals.selectedDay.ToLower()))
                     {
+                        // Skip if not the selected day
                         continue;
                     }
-                    var dayAttributes = day.Elements();
-                    foreach (var dayAttribute in dayAttributes) // Loop through day attributes
-                    {
-                        if (dayAttribute.Name.LocalName.ToString().Contains("title"))
-                        {
-                            if (!dayAttribute.Value.ToString().ToLower().Contains(Globals.selectedDay.ToLower()))
-                            {
-                                // Skip if not the selected day
-                                break;
-                            }
-                            correctDayFound = true;
-                        }
-                        else if (dayAttribute.Name.LocalName.ToString().Contains("description"))
-                        {
-                            restaurantMenu = dayAttribute.Value.ToString();
-                        }
-                        else
-                        {
-                            continue;
-                        }
-                    }
-                    if (correctDayFound)
-                    {
-                        break;
-                    }
-                };
-                this.menu = PurifyMenu(restaurantMenu);
+                    menu = CleanMenu(dayNode.Element("description").Value.ToString());
+                    break;
+                }
             }
             catch
             {
@@ -83,27 +59,6 @@ namespace Edumenu.Models
             }
         }
 
-        private void UpdateProgress()
-        {
-            Globals.nRestaurantsProcessed += 1;
-            int totalSchoolRestaurants = 0;
-            foreach (Restaurant restaurant in App.RestaurantViewModel.restaurantsAll)
-            { 
-                if (restaurant.school.name_fi.Equals(school.name_fi))
-                {
-                    totalSchoolRestaurants += 1;
-                }
-            }
-            if (totalSchoolRestaurants != 0) // Do not divide by zero
-            { 
-                Globals.progress = (int)((double)Globals.nRestaurantsProcessed / (double)totalSchoolRestaurants * 100);
-            }
-            if (Globals.nRestaurantsProcessed.Equals(totalSchoolRestaurants))
-            {
-                Globals.allRestaurantsProcessed = true;
-            }
-        }
-
         internal void ParseCampusRavita(string p)
         {
             for (int i=0; i< 10*10; ++i)
@@ -113,13 +68,47 @@ namespace Edumenu.Models
             UpdateProgress();
         }
 
-        internal void ParseJuvenes(string p)
+        internal void ParseJuvenes(string sourceCode)
         {
-            for (int i = 0; i < 10 *10*10* 10 * 10; ++i)
+            try
             {
-                continue;
+                string dayOfWeek = "";
+                string dayOfWeekAndMenu = "";
+                string dayOfWeekEndSequence = "</strong></p>";
+
+                XElement rss = XElement.Parse(sourceCode);
+                var dayNodes = rss.Element("channel").Elements("item");
+                foreach (XElement dayNode in dayNodes)
+                {
+                    dayOfWeekAndMenu = dayNode.Element("description").Value.ToString();
+                    if (!dayOfWeekAndMenu.Contains(dayOfWeekEndSequence))
+                    {
+                        // Skip if not able to resolve the day of week
+                        continue;
+                    }
+                    dayOfWeek = dayOfWeekAndMenu.Substring(0,
+                        dayOfWeekAndMenu.IndexOf(dayOfWeekEndSequence) +
+                        dayOfWeekEndSequence.Length);
+                    if (!dayOfWeek.ToLower().Contains(Globals.selectedDay.ToLower()))
+                    {
+                        // Skip if not the selected day
+                        continue;
+                    }
+                    menu = CleanMenu(dayOfWeekAndMenu.Substring(
+                        dayOfWeekAndMenu.IndexOf(dayOfWeekEndSequence) +
+                        dayOfWeekEndSequence.Length));
+                    break;
+                }
             }
-            UpdateProgress();
+            catch
+            {
+                // Silently swallow an error condition and continue execution.
+                // This is okay, because menu strings were initialized properly before execution.
+            }
+            finally
+            {
+                UpdateProgress();
+            }
         }
 
         internal void ParseSodexo(string p)
@@ -131,20 +120,83 @@ namespace Edumenu.Models
             UpdateProgress();
         }
 
-        private string PurifyMenu(string dirtyMenu)
+        private string CleanMenu(string dirtyMenu)
         {
-            dirtyMenu = dirtyMenu.Replace("&lt;br&gt;", "");
-            dirtyMenu = dirtyMenu.Replace("<br>", "");
-            // Remove whitespace inside curly and square brackets
-            //  \s +     # Match whitespace
-            //  (?=      # only if followed by...
-            //  [^ ()] * # any number of characters except parentheses
-            //  \)       # and a closing parenthesis
-            //  )        # End of lookahead assertion
-            dirtyMenu = Regex.Replace(dirtyMenu, "\\s+(?=[^()]*\\))", "");
-            //dirtyMenu = Regex.Replace(dirtyMenu, "\\s+(?=[^[]]*\\])", "");
+            switch (company)
+            {
+                case Company.Amica:
+                    dirtyMenu = dirtyMenu.Replace("&lt;br&gt;", "");
+                    dirtyMenu = dirtyMenu.Replace("<br>", "");
+
+                    // Remove whitespace inside curly and square brackets
+                    //  \s +     # Match whitespace
+                    //  (?=      # only if followed by...
+                    //  [^ ()] * # any number of characters except parentheses
+                    //  \)       # and a closing parenthesis
+                    //  )        # End of lookahead assertion
+                    dirtyMenu = Regex.Replace(dirtyMenu, "\\s+(?=[^()]*\\))", "");
+                    //dirtyMenu = Regex.Replace(dirtyMenu, "\\s+(?=[^[]]*\\])", "");
+
+                    // Remove uppercase from menu type title
+                    // Split by newlines
+                    string[] lines = dirtyMenu.Split(new string[]
+                        { "\r\n", "\n" }, StringSplitOptions.None);
+                    dirtyMenu = String.Empty;
+                    foreach (string line in lines)
+                    {
+                        if (line.Count(f => f == ':').Equals(1))
+                        {
+                            dirtyMenu += FirstCharToUpper(line);
+                            continue;
+                        }
+                        dirtyMenu += line + Environment.NewLine;
+                    }
+                    break;
+                case Company.Juvenes:
+                    dirtyMenu = dirtyMenu.Replace("<ul>", "").Replace("</ul>", "");
+                    dirtyMenu = dirtyMenu.Replace("<strong>", Environment.NewLine);
+                    dirtyMenu = dirtyMenu.Replace("</strong>", "");
+                    // The case when one food consists of multiple elements
+                    dirtyMenu = dirtyMenu.Replace("</li><li>", Environment.NewLine);
+                    dirtyMenu = dirtyMenu.Replace("<li>", Environment.NewLine);
+                    dirtyMenu = dirtyMenu.Replace("</li>", Environment.NewLine);
+                    dirtyMenu = dirtyMenu.Replace("&nbsp;", " ").Trim();
+                    break;
+            }
             string cleanMenu = dirtyMenu.Trim();
             return cleanMenu;
+        }
+
+        public static string FirstCharToUpper(string input)
+        {
+            if (String.IsNullOrEmpty(input))
+            {
+                return input;
+            }
+            return input.First().ToString().ToUpper()
+                + input.Substring(1).ToLower() + Environment.NewLine;
+        }
+
+        private void UpdateProgress()
+        {
+            Globals.nRestaurantsProcessed += 1;
+            int totalSchoolRestaurants = 0;
+            foreach (Restaurant restaurant in App.RestaurantViewModel.restaurantsAll)
+            {
+                if (restaurant.school.name_fi.Equals(school.name_fi))
+                {
+                    totalSchoolRestaurants += 1;
+                }
+            }
+            if (totalSchoolRestaurants != 0) // Do not divide by zero
+            {
+                Globals.progress = (int)((double)Globals.nRestaurantsProcessed /
+                    (double)totalSchoolRestaurants * 100);
+            }
+            if (Globals.nRestaurantsProcessed.Equals(totalSchoolRestaurants))
+            {
+                Globals.allRestaurantsProcessed = true;
+            }
         }
 
     }
