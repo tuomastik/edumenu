7fc5a582f9ca7168050ff3e1583e12bb1e298c44
Tuomas Tikkanen <tuomas.tikkanen@hotmail.com>
2015-09-15 02:17:20 +0300

    Initial commit.

---
+++ Edumenu/Models/Utils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edumenu.Models
+{
+    class Utils
+    {
+        public static string FirstCharToUpper(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return input;
+            }
+            return input.First().ToString().ToUpper() + input.Substring(1).ToLower();
+        }
+    }
+}
