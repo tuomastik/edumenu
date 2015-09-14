4a31b22cab42bda30e1acc825e2be94bf5520fb0
Tuomas Tikkanen <tuomas.tikkanen@hotmail.com>
2015-09-14 09:35:16 +0300

    Initial commit.

---
+++ Edumenu/Models/Diet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edumenu.Models
+{
+    public class Diet
+    {
+        public string name_en { get; set; }
+        public string name_fi { get; set; }
+        public string nameShort_en { get; set; }
+        public string nameShort_fi { get; set; }
+        public Uri iconPath { get; set; }
+
+        public static Diet glutenFree = new Diet
+        {
+            name_en = "Gluten-free",
+            name_fi = "Gluteeniton",
+            nameShort_fi = "G",
+        };
+
+    }
+}
---
+++ Edumenu/Models/Diets.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edumenu.Models
+{
+    public class Diets
+    {
+        //public static
+    }
+}
