181d7ac44c65244894ff74f3462bcac73faafd9b
Tuomas Tikkanen <tuomas.tikkanen@hotmail.com>
2015-09-16 00:49:01 +0300

    Now adding restaurants manually instead of automatically, so that restaurants will be added in correct order (the order in which they will be presented in the app.

--- Edumenu/ViewModels/RestaurantViewModel.cs
+++ Edumenu/ViewModels/RestaurantViewModel.cs
@@ -16,14 +16,37 @@ namespace Edumenu.ViewModels
             this.restaurantsVisible = new ObservableCollection<Restaurant>();
             this.restaurantsAll = new ObservableCollection<Restaurant>();
 
-            // Add all the restaurants from the Restaurant class to the observablecollection
-            var bindingFlags = BindingFlags.Static | BindingFlags.Public;
-            Type myType = typeof(Restaurant);
-            FieldInfo[] fields = myType.GetFields(bindingFlags);
-            foreach (FieldInfo field in fields)
-            {
-                restaurantsAll.Add((Restaurant)field.GetValue(this));
-            }
+            // Restaurants will be presented in the same order that
+            // they are added to the restaurantsAll
+
+            // TUT
+            restaurantsAll.Add(Restaurant.hertsi);
+            restaurantsAll.Add(Restaurant.reaktori);
+            restaurantsAll.Add(Restaurant.newton);
+            restaurantsAll.Add(Restaurant.newtonRoheeXtra);
+            // UTA
+            restaurantsAll.Add(Restaurant.yliopistonRavintola);
+            restaurantsAll.Add(Restaurant.minerva);
+            restaurantsAll.Add(Restaurant.linna);
+            restaurantsAll.Add(Restaurant.pinni);
+            restaurantsAll.Add(Restaurant.salaattibaari);
+            restaurantsAll.Add(Restaurant.utaFusionKitchen);
+            restaurantsAll.Add(Restaurant.roheeXtra);
+            restaurantsAll.Add(Restaurant.intro);
+            restaurantsAll.Add(Restaurant.cafeAlakuppila);
+            // TAYS
+            restaurantsAll.Add(Restaurant.medicaBio);
+            restaurantsAll.Add(Restaurant.medicaArvo);
+            restaurantsAll.Add(Restaurant.medicaArvoFusionKitchen);
+            // TAMK
+            restaurantsAll.Add(Restaurant.campusravita);
+            restaurantsAll.Add(Restaurant.pirteria);
+            restaurantsAll.Add(Restaurant.ziberia);
+            // TAKK
+            restaurantsAll.Add(Restaurant.nasta);
+            restaurantsAll.Add(Restaurant.nastaFusionKitchen);
+            restaurantsAll.Add(Restaurant.ratamo);
+
         }
     }
 }
