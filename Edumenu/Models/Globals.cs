e2ad7b374f0534998ab006c93b2e39335fef9644
Tuomas Tikkanen <t-tuoti@microsoft.com>
2015-09-16 17:54:29 +0300

    Moved from MainPage.xaml.cs to own file

---
+++ Edumenu/Models/Globals.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edumenu.Models
+{
+    public class Globals : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private static string selectedDay = Utils.FirstCharToUpper(new CultureInfo("fi-FI").
+            DateTimeFormat.GetDayName(DateTime.Today.DayOfWeek));
+
+        public static string SelectedDay
+        {
+            get
+            {
+                return selectedDay;
+            }
+            set
+            {
+                selectedDay = value;
+                OnPropertyChanged("SelectedDay");
+            }
+        }
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+
+        
+
+
+        public static int progress; // Total progress 0...1
+        public static int nRestaurantsProcessed; // Restaurants processed per school
+        public static bool allRestaurantsProcessed; // Can we exit the background thread?
+    }
+}
