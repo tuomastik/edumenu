19045f69f21fd7cb61d25f4467ab547a1f3f857a
Tuomas Tikkanen <t-tuoti@microsoft.com>
2015-09-17 15:58:50 +0300

    Added Day.cs

---
+++ Edumenu/Models/Day.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edumenu.Models
+{
+    class Day : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //private static string selected = Utils.FirstCharToUpper(new CultureInfo("fi-FI").
+        //    DateTimeFormat.GetDayName(DateTime.Today.DayOfWeek));
+
+        private string name = string.Empty;
+        private bool isSelected = false;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public bool IsSelected
+        {
+            get
+            {
+                return isSelected;
+            }
+            set
+            {
+                isSelected = value;
+                OnPropertyChanged("IsSelected");
+            }
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
