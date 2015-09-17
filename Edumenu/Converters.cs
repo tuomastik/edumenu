d334abc6f7dd6544d2c681f822fda8c330d4c9bf
Tuomas Tikkanen <tuomas.tikkanen@hotmail.com>
2015-09-17 22:25:36 +0300

    Added SelectedDayToFontWeight

--- Edumenu/Converters.cs
+++ Edumenu/Converters.cs
@@ -15,15 +15,35 @@ namespace Edumenu
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (string.IsNullOrEmpty((string)value))
+            if (value.Equals(null))
             {
                 return new SolidColorBrush(Colors.White);
             }
-            if (((string)value).ToLower().Equals(Globals.SelectedDay.ToLower()))
+            if ((bool)value)
             {
-                return Application.Current.Resources["ThemeColor4"];
+                return Application.Current.Resources["ThemeColor1"];
             }
             return new SolidColorBrush(Colors.White);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+    public class SelectedDayToFontWeight : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value.Equals(null))
+            {
+                return FontWeights.Normal;
+            }
+            if ((bool)value)
+            {
+                return FontWeights.SemiBold;
+            }
+            return FontWeights.Normal;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
