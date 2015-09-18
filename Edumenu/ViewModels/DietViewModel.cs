ca05fd0b0aa9481166204784f77e7a8745121f91
Tuomas Tikkanen <tuomas.tikkanen@hotmail.com>
2015-09-19 02:47:56 +0300

    Added diets ObservableCollection

--- Edumenu/ViewModels/DietViewModel.cs
+++ Edumenu/ViewModels/DietViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Edumenu.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,33 @@ namespace Edumenu.ViewModels
 {
     public class DietViewModel
     {
+        public ObservableCollection<Diet> diets { get; set; }
+
+        public DietViewModel()
+        {
+            diets = new ObservableCollection<Diet>()
+            {
+                Diet.glutenFree,
+                Diet.milkFree,
+                Diet.lactoseFree,
+                Diet.lowLactose,
+                Diet.vegetarian,
+                Diet.vegan,
+                Diet.lite,
+                Diet.hot,
+                Diet.withoutEgg,
+                Diet.withPork,
+                Diet.withBeef,
+                Diet.withFish,
+                Diet.withGarlic,
+                Diet.withSweetPepper,
+                Diet.withCitrus,
+                Diet.withNuts,
+                Diet.withCelery,
+                Diet.withSoy,
+                Diet.withAllergens,
+                Diet.nutritionalGuidelines
+            };
+        }
     }
 }
