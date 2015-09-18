c333939ad30f64afa96bb6c814b20744f7babb4e
Tuomas Tikkanen <tuomas.tikkanen@hotmail.com>
2015-09-18 09:34:44 +0300

    Added IsSelected property

--- Edumenu/Models/School.cs
+++ Edumenu/Models/School.cs
@@ -1,11 +1,37 @@
-﻿namespace Edumenu.Models
+﻿using System.ComponentModel;
+
+namespace Edumenu.Models
 {
-    public class School
+    public class School : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public string Name_FI { get; set; }
         public string Name_EN { get; set; }
         public string NameShort_EN { get; set; }
         public string NameShort_FI { get; set; }
+
+        private bool isSelected = false;
+        public bool IsSelected
+        {
+            get
+            {
+                return isSelected;
+            }
+            set
+            {
+                isSelected = value;
+                OnPropertyChanged("IsSelected");
+            }
+        }
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
 
         public static School tut = new School
         {
