5bf033c657d57f6d52fa093336600df54e9a696c
Tuomas Tikkanen <tuomas.tikkanen@hotmail.com>
2015-09-30 09:52:51 +0300

    Renamed button click

--- Edumenu/ChangeLog.xaml.cs
+++ Edumenu/ChangeLog.xaml.cs
@@ -1,30 +1,12 @@
 ﻿using Edumenu.Models;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI;
 using Windows.UI.Core;
-using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
 
 namespace Edumenu
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class ChangeLog : Page
     {
         public ChangeLog()
@@ -42,7 +24,7 @@ namespace Edumenu
             Utils.ConfigureStatusBar();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void ContinueToMainPage_Click(object sender, RoutedEventArgs e)
         {
             this.NavigateWithDispatcher(Window.Current.Content as Frame, typeof(MainPage));
         }
@@ -58,15 +40,6 @@ namespace Edumenu
             // crash "(0xc0000005) Access violation"
             await Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
                 () => frame.Navigate(typeOfPage, "ChangeLog"));
-        }
-
-        /// <summary>
-        /// Invoked when this page is about to be displayed in a Frame.
-        /// </summary>
-        /// <param name="e">Event data that describes how this page was reached.
-        /// This parameter is typically used to configure the page.</param>
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
         }
     }
 }
